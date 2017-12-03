06b0abe5d6396e234be1fd7bc61a6b45bc759adc
Pierre Allard <pierallard@gmail.com>
2017-12-03 18:02:51 +0100

    Animate leaves

--- src/world/Leaf.ts
+++ src/world/Leaf.ts
@@ -1,38 +1,43 @@
 const SCALE_MIN = 0.3;
 const SCALE_MAX = 0.6;
 const LEAF_COUNT = 7;
-const RADIUS_MAX = 100;
-const MAX_ROTATION = Math.PI / 4;
+const GAP_HEIGHT = 150;
+const GAP_WIDTH = 100;
+const MAX_ROTATION = Math.PI / 5;
 
 export class Leaf extends Phaser.Sprite
 {
+    private tween: Phaser.Tween;
+    private group: Phaser.Group;
+
     constructor(group: Phaser.Group, x: number, y: number)
     {
         const leafnumber = 1 + Math.floor(Math.random() * LEAF_COUNT);
-        const xRandom = x + (Math.random() * RADIUS_MAX) - (RADIUS_MAX / 2);
-        const yRandom = y + (Math.random() * RADIUS_MAX) - (RADIUS_MAX / 2);
+        const xRandom = x + (Math.random() * GAP_WIDTH) - (GAP_WIDTH / 2);
+        const yRandom = y + (Math.random() * GAP_HEIGHT) - (GAP_HEIGHT / 2);
         super(group.game, xRandom, yRandom, 'leaf' + leafnumber, 0);
 
-        group.game.physics.enable(this, Phaser.Physics.ARCADE);
         group.add(this);
+        this.group = group;
 
-        this.inputEnabled = true;
-        this.anchor.setTo(0.5, 0.5);
+        this.anchor.setTo(0.5, 0.2);
 
         const scale = Math.random() * (SCALE_MAX - SCALE_MIN) + SCALE_MIN;
         this.scale.setTo(scale, scale);
         this.rotation = Math.random() * MAX_ROTATION * 2 - MAX_ROTATION;
 
-        this.body.allowGravity = false;
-        this.body.collideWorldBounds = true;
+        this.updateTween();
     }
 
-    public pickable(): boolean
-    {
-        return false;
+    runAnimation() {
+        if (this.tween.isRunning) {
+            return null;
+        }
+        this.tween.start();
     }
 
-    public hit()
-    {
+    updateTween() {
+        this.tween = this.group.game.add.tween(this).to( { rotation: Math.random() * MAX_ROTATION * 2 - MAX_ROTATION }, 100, Phaser.Easing.power2, false);
+        this.tween.onComplete.add(this.updateTween, this);
     }
 }
